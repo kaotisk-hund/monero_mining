533bd7c6bffe3e1d0591c09375332d46ef515132
kaotisk <kaotisk@arching-kaos.com>
2021-05-13 12:35:18 +0300

    Uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "XMRig and XMRig proxy uninstaller"
+echo "---------------------------------"
+echo "Deleting symlink... (need root)"
+sudo rm /usr/local/bin/xmrig
+sudo rm /usr/local/bin/xmrig-proxy
