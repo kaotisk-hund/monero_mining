34c1d0eca1185b963937b23684875bf6cbb2db5b
kaotisk <kaotisk@arching-kaos.com>
2021-05-14 11:01:44 +0300

    Fixed typo at xmrig proxy link creation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Launching updater to get latest version of XMRig and XMRig proxy from gith
 sh update.sh
 echo "Creating symlinks... (need root)"
 sudo ln -s $PWD/downloads/xmrig-latest/xmrig /usr/local/bin/xmrig
-sudo ln -s $PWD/downloads/xmrig-proxy-latest/xmrig-proxy /usr/local/bin/xmrig
+sudo ln -s $PWD/downloads/xmrig-proxy-latest/xmrig-proxy /usr/local/bin/xmrig-proxy
 echo "Configuring in donate mode..."
 sh configure.sh --donate
 echo "You can now start xmrig and let it run while you read the rest of the docs"
