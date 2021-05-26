7807fc05aca451f8e0d09fb4bfba8a35614392c4
kaotisk <kaotisk@arching-kaos.com>
2021-05-26 03:22:54 +0300

    Added more commands to system so we can do from which ever path

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ sh update.sh
 echo "Creating symlinks... (need root)"
 sudo ln -s $PWD/downloads/xmrig-latest/xmrig /usr/local/bin/xmrig
 sudo ln -s $PWD/downloads/xmrig-proxy-latest/xmrig-proxy /usr/local/bin/xmrig-proxy
+sudo ln -s $PWD/tools/mine-looper.sh /usr/local/bin/monero-mining
+sudo ln -s $PWD/tools/proxy-looper.sh /usr/local/bin/monero-proxy
+sudo ln -s $PWD/update.sh /usr/local/bin/monero-mining-update
+sudo ln -s $PWD/configure.sh /usr/local/bin/monero-mining-configure
 echo "Configuring in donate mode..."
 sh configure.sh --donate
 echo "You can now start xmrig and let it run while you read the rest of the docs"
