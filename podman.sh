27fa19c2b65fb9455a2ab3d156828072b7acf728
kaotisk <kaotisk@arching-kaos.com>
2021-12-03 06:21:03 +0200

    Fix on launch command

--- podman.sh
+++ podman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 podman build -f ContainerFile -t monero-mining .
-podman start -d --name monero-miner --privileged --network=host monero-mining
+podman run -d --name monero-miner --privileged --network=host monero-mining
 SERVICE=$(podman generate systemd --files --name monero-miner)
 systemctl enable --user SERVICE
 podman stop monero-miner
